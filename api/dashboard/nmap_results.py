32d7832572cc8f09f35f378535474352db520dba
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-11-21 22:53:51 +0530

    finer data

--- api/dashboard/nmap_results.py
+++ api/dashboard/nmap_results.py
@@ -2,7 +2,6 @@ import json
 import os
 
 from boto3.dynamodb.conditions import Key
-from operator import itemgetter
 
 from utils.dynamodb import DynamoDbWrapper
 from utils.decorators import cors_headers
@@ -19,23 +18,29 @@ def handler(event, context):
     """
     nmap_results = dynamodb.query(
         table_name=table_name,
-        query=Key('category').eq('nmap_results'),
+        query=Key('category').eq('nmap_result'),
     )
 
     if nmap_results:
-        open_ports = sorted(nmap_results['Hosts'][0]['Ports'], key=lambda i: i['State'].lower() == 'open')
-        ports_details = []
-        for items in open_ports:
-            port = {
-                items['ID']: items['Protocol']
-            }
-            ports_details.append(port)
+        scan_details = []
+        for results in nmap_results:
+            host = json.loads(results['value'])['hosts'][0]
+            for ports in host['ports']:
+                for script_output in ports['scripts']:
+                    item = {
+                    'host': host,
+                    'port': ports['id'],
+                    'protocol': ports['protocol'],
+                    'service': ports['service'],
+                    'script': script_output['output']
+                    }
+                    scan_details.append(item)
 
         return {
             'statusCode': 200,
             'body': json.dumps({
                 'data': {
-                    'nmap_results': ports_details,
+                    'nmap_results': scan_details,
                 }
             })
         }
